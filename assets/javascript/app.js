96c0fc9f085b18517c57e80a3f1cf26eca828628
Mohammed Alnasseri <mohammedalnasseri@Mohammeds-MacBook-Pro.local>
2018-01-30 23:55:08 -0500

    added scroll up function on reload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,4 @@
-$(document).ready(function(){
-    $('html, body').scrollTop(0);
 
-    $(window).on('load', function() {
-    setTimeout(function(){
-        $('html, body').scrollTop(0);
-    }, 0);
- });
-});
   function smoothScroll(target, time) {
   var margin = ($('#main-header').outerHeight() - 1);
   if (!time) { time = '1000'; }
